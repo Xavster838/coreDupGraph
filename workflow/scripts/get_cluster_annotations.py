8a0721652ab1e80d2306aea5c9d9eaeacb8d33f4
Xavster838 <francisguitart@gmail.com>
2022-07-28 16:20:04 -0700

    added score to specify weight of each dup in get_cluster_annotations.py.

--- workflow/scripts/get_cluster_annotations.py
+++ workflow/scripts/get_cluster_annotations.py
@@ -10,7 +10,7 @@ df = pd.read_csv(snakemake.input.mapping_stats , sep = "\t")
 #make network
 dup_net = nx.from_pandas_edgelist(df, source = "ref_loc_name", target="q_dup_name", edge_attr="score")
 #get louvain clusterings.
-comms = community_louvain.best_partition(dup_net)
+comms = community_louvain.best_partition(dup_net, weight ='score')
 #output to dataframe
 out_df = pd.DataFrame(data = [comms.keys(), comms.values()]).transpose()
 out_df.columns = ['dup', 'cluster']
